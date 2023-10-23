c86b62561bd6fa20f8ea298e588ecc5d8d15e7da
John-Petrangelo <john@petrangelo.org>
2023-10-23 00:02:10 -0400

    Use keypad interface instead of raw buttons

--- Dial.py
+++ Dial.py
@@ -1,34 +1,23 @@
-import time
 import rotaryio
 import board
-import digitalio
 import keypad
 
 
-##### Keypad info
+# CONFIGURATION
 
-#keys = keypad.Keys((board.D5,), value_when_pressed=False, pull=True, interval=0.020)
+mic_gp = board.GP13
+cam_gp = board.GP2
+mode_gp = board.GP9
 
-#while True:
-#    event = keys.events.get()
-#    # event will be None if nothing has happened.
-#    if event:
-#        print(event)
-#        event.key_number - keys are numbered starting at 0
-#        event.pressed
-#        event.released
+mic_key = 0
+cam_key = 1
+mode_key = 2
 
+mode_teams = 1
+mode_zoom = 0
 
-def setup_button(pin):
-    button = digitalio.DigitalInOut(pin)
-    button.direction = digitalio.Direction.INPUT
-    button.pull = digitalio.Pull.UP
-    return button
-    
-
-def print_state():
-        print(f"Volume={new_volume} Mic={new_mic_state:1}  Cam={new_cam_state:1}  Mode={new_mode_state:1}")
-
+# default mode is Zoom - if switch is in Teams position then we'll get a "pressed" event on startup
+mode = mode_zoom
 
 # MAIN PROGRAM
 
@@ -37,51 +26,23 @@ print("Dial starting up")
 rot_enc = rotaryio.IncrementalEncoder(board.GP20, board.GP21, 2)
 last_volume = 0
 
-cam_button = setup_button(board.GP2)
-last_cam_state = None
-
-mic_button = setup_button(board.GP13)
-last_mic_state = None
-
-mode_switch = setup_button(board.GP9)
-last_mode_state = None
+keys = keypad.Keys((mic_gp, cam_gp, mode_gp), value_when_pressed=False, pull=True, interval=0.100)
 
 while True:
     new_volume = rot_enc.position
-    if new_volume >= last_volume:
-        # Volume up
-        pass
-    if new_volume <= last_volume:
-        # Volume down
-        pass
+    if new_volume > last_volume:
+        print(f"volume up ({new_volume})")
+    if new_volume < last_volume:
+        print(f"volume down ({new_volume})")
+    last_volume = new_volume
     
-    new_cam_state = not cam_button.value
-    if last_cam_state is None or new_cam_state != last_cam_state:
-        # Camera on/off button pressed
-
-        # Poor man's debounce
-        time.sleep(0.1)
-    
-    new_mic_state = not mic_button.value
-    if last_mic_state is None or new_mic_state != last_mic_state:
-        # Microphone on/off button pressed
-
-        # Poor man's debounce
-        time.sleep(0.1)
-    
-    new_mode_state = not mode_switch.value
-    if last_mode_state is None or new_mode_state != last_mode_state:
-        # Mode switch switched
-
-        # Poor man's debounce
-        time.sleep(0.1)
-
-    if [last_volume, last_cam_state, last_mic_state, last_mode_state] != [new_volume, new_cam_state, new_mic_state, new_mode_state]:
-        print_state()
-
-        # Save all of the new states
-        last_volume = new_volume
-        last_mic_state = new_mic_state
-        last_cam_state = new_cam_state
-        last_mode_state = new_mode_state
-
+    event = keys.events.get()
+    if event:
+        if event.key_number == mic_key and event.pressed:
+            print("mic pressed")
+        if event.key_number == cam_key and event.pressed:
+            print("cam pressed")
+        if event.key_number == mode_key and event.pressed:
+            print("mode=Teams")
+        if event.key_number == mode_key and event.released:
+            print("mode=Zoom")
