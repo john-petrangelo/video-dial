9342e9dbb3b65e657a75506bf51c8e2a7c08dd2f
John-Petrangelo <john@petrangelo.org>
2023-10-23 01:12:21 -0400

    Added code to send HID events
    - Volume up/down now works
    - Sends appropriate keys, except COMMAND key omitted for now for debug purposes
    - Added more comments, minor code improvements

--- Dial.py
+++ Dial.py
@@ -2,47 +2,83 @@ import rotaryio
 import board
 import keypad
 
+import usb_hid
+from adafruit_hid.consumer_control import ConsumerControl
+from adafruit_hid.consumer_control_code import ConsumerControlCode
+from adafruit_hid.keyboard import Keyboard
+from adafruit_hid.keycode import Keycode
 
 ### CONFIGURATION
 
+# GPIO pin assignements
 mic_gp = board.GP13
 cam_gp = board.GP2
 mode_gp = board.GP9
 
+# Keypad indices
 mic_key = 0
 cam_key = 1
 mode_key = 2
 
-mode_teams = 1
-mode_zoom = 0
-
-# default mode is Zoom - if switch is in Teams position then we'll get a "pressed" event on startup
+# Modes (Teams or Zoom)
+# The default mode is Zoom - if switch is in Teams position then we'll get a "pressed" event on startup
+mode_teams = "Teams"
+mode_zoom = "Zoom"
 mode = mode_zoom
+
+
+def toggle_mic():
+    print("toggling mic")
+    if mode == mode_teams:
+        #kbd.send(Keycode.COMMAND, Keycode.SHIFT, Keycode.M)
+        kbd.send(Keycode.SHIFT, Keycode.M)
+    else:
+        #kbd.send(Keycode.COMMAND, Keycode.SHIFT, Keycode.A)
+        kbd.send(Keycode.SHIFT, Keycode.A)
+
+
+def toggle_cam():
+    print("toggling cam")
+    if mode == mode_teams:
+        #kbd.send(Keycode.COMMAND, Keycode.SHIFT, Keycode.O)
+        kbd.send(Keycode.SHIFT, Keycode.O)
+    else:
+        #kbd.send(Keycode.COMMAND, Keycode.SHIFT, Keycode.V)
+        kbd.send(Keycode.SHIFT, Keycode.V)
+
 
 ### MAIN PROGRAM
 
+
 print("Dial starting up")
 
+# Setup the USB HID devices
+consumer_control = ConsumerControl(usb_hid.devices)
+kbd = Keyboard(usb_hid.devices)
+
 rot_enc = rotaryio.IncrementalEncoder(board.GP20, board.GP21, 2)
-last_volume = 0
+last_position = 0
 
 keys = keypad.Keys((mic_gp, cam_gp, mode_gp), value_when_pressed=False, pull=True, interval=0.100)
 
 while True:
-    new_volume = rot_enc.position
-    if new_volume > last_volume:
-        print(f"volume up ({new_volume})")
-    if new_volume < last_volume:
-        print(f"volume down ({new_volume})")
-    last_volume = new_volume
+    # Handle changes to the rotary encoder
+    new_position = rot_enc.position
+    if new_position > last_position:
+        print(f"volume up ({new_position})")
+        consumer_control.send(ConsumerControlCode.VOLUME_INCREMENT)
+    if new_position < last_position:
+        print(f"volume down ({new_position})")
+        consumer_control.send(ConsumerControlCode.VOLUME_DECREMENT)
+    last_position = new_position
     
+    # Handle button presses and switch toggles
     event = keys.events.get()
     if event:
         if event.key_number == mic_key and event.pressed:
-            print("mic pressed")
+            toggle_mic()
         if event.key_number == cam_key and event.pressed:
-            print("cam pressed")
-        if event.key_number == mode_key and event.pressed:
-            print("mode=Teams")
-        if event.key_number == mode_key and event.released:
-            print("mode=Zoom")
+            toggle_cam()
+        if event.key_number == mode_key:
+            mode = mode_teams if event.pressed else mode_zoom
+            print(f"Setting mode to {mode}")
